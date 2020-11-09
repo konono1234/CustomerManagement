8ee87e9f40fd561e876c1d63d6f52c6a10abc180
konoue <yuki.konoue@comsize.com>
2020-11-09 13:02:45 +0900

    土台作り完成。しかし、mybatismapperが無いよと警告される

--- src/main/java/com/customer/controller/CustomerController.java
+++ src/main/java/com/customer/controller/CustomerController.java
@@ -1,5 +1,31 @@
 package com.customer.controller;
 
-public class CustomerController {
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import com.customer.bean.CustomerBean;
+import com.customer.service.CustomerService;
 
+
+/*
+ * Beanにデータを受け渡したりServiceを起動するコントローラークラスです
+ * 
+ */
+
+@Controller
+public class CustomerController {
+	
+	@Autowired
+	private CustomerService customerService;
+	
+	@RequestMapping(value = "index")
+	public String init(Model model) {
+		
+		List<CustomerBean> list = customerService.selectIndex();
+		model.addAttribute("list", list);
+		
+		return "index";
+	}
 }
--- src/main/java/com/customer/service/CustomerService.java
+++ src/main/java/com/customer/service/CustomerService.java
@@ -1,5 +1,25 @@
 package com.customer.service;
 
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.customer.bean.CustomerBean;
+import com.customer.mapper.CustomerMapper;
+
+/*
+ * Mapperクラスを起動するサービスクラスです。
+ * 
+ * 
+ */
+
+@Service
 public class CustomerService {
+	
+	@Autowired
+	private CustomerMapper customerMapper;
+	
+	public List<CustomerBean> selectIndex(){
+		return customerMapper.selectIndexAll();
+	}
 
 }
