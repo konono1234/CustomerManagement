7d509778fbfcfc44f8ff520e6ea27860def9fe9f
konoue <yuki.konoue@comsize.com>
2020-11-09 16:03:55 +0900

    mapperエラー前

--- src/main/java/com/customer/CustomermanagementApplication.java
+++ src/main/java/com/customer/CustomermanagementApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.customer;
 
 import javax.sql.DataSource;
 
--- src/main/java/com/customer/web/IndexController.java
+++ src/main/java/com/customer/web/IndexController.java
@@ -1,5 +1,22 @@
 package com.customer.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.customer.web.IndexController;
+
+@Controller
 public class IndexController {
+
+	final static Logger logger = LoggerFactory.getLogger(IndexController.class);
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String index() {
+
+		return "redirect:/index";
+	}
 
 }
