2b889d6406a7da9bce086121368f028c644340d2
komiya <hiroyuki.komiya@comsize.com>
2020-11-11 11:30:28 +0900

    コリジョン修正

--- src/main/java/com/customer/service/CustomerService.java
+++ src/main/java/com/customer/service/CustomerService.java
@@ -1,29 +1,28 @@
 package com.customer.service;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.customer.bean.CustomerBean;
 import com.customer.mapper.CustomerMapper;
 
 /*
  * Mapperクラスを起動するサービスクラスです。
- * 
- * 
+ *
+ *
  */
 
 @Service
 
 public class CustomerService {
-	
-	@Autowired
-	CustomerMapper customerMapper;
-	
-	public List<CustomerBean> selectIndex(){
-		List<CustomerBean> customerList = customerMapper.selectIndexAll();
-		return customerList;
-	}
+
+  @Autowired
+  CustomerMapper customerMapper;
+
+  public List<CustomerBean> selectIndex() {
+    List<CustomerBean> customerList = customerMapper.selectIndexAll();
+    return customerList;
+    // return customerMapper.selectIndexAll();
+  }
 
 }
--- src/main/java/com/customer/web/admin/AdminCustomerController.java
+++ src/main/java/com/customer/web/admin/AdminCustomerController.java
@@ -1,40 +1,36 @@
 package com.customer.web.admin;
 
 import java.util.List;
-import java.util.Locale;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
 import com.customer.bean.CustomerBean;
 import com.customer.service.CustomerService;
 
+@Controller
 public class AdminCustomerController {
-//	final static Logger logger = LoggerFactory.getLogger(AdminCustomerController.class);
-	
-	@Autowired
-	CustomerService customerService;
-	
-	@Autowired
-	MessageSource msg;
-	
-	@RequestMapping(value = "/admin/customer", method = RequestMethod.GET )
-	public String index(Model model) {
-		List<CustomerBean> list = customerService.selectIndex();
-		
-		/*
-		 * if(CollectionUtils.isEmpty(list)) {
-			String message = msg.getMessage("customer.list.empty", null, Locale.JAPANESE); 
-			model.addAttribute("emptyMessage", message);
-		}
-		*/
-		model.addAttribute("list", list);
-		
-		return "admin/customer/index";
-	}
+  // final static Logger logger = LoggerFactory.getLogger(AdminCustomerController.class);
+
+  @Autowired
+  CustomerService customerService;
+
+  @Autowired
+  MessageSource msg;
+
+  @RequestMapping(value = "/admin/customer", method = RequestMethod.GET)
+  public String index(Model model) {
+    List<CustomerBean> list = customerService.selectIndex();
+
+    /*
+     * if(CollectionUtils.isEmpty(list)) { String message = msg.getMessage("customer.list.empty",
+     * null, Locale.JAPANESE); model.addAttribute("emptyMessage", message); }
+     */
+    model.addAttribute("list", list);
+
+    return "admin/customer/index";
+  }
 
 }
